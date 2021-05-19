40466914a47df840dacc3b1e73b372ebdf396a5b
LazarGrbovic <lazarmgrbovic@gmail.com>
2021-05-19 14:52:40 +0200

    Refactored Implementation
    * Now two comma separated numbers can be provided, and their sum will be returned

--- StringCalculator/StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -7,8 +7,14 @@ namespace StringCalculator
         public static int Add(string numbers)
         {
             if(string.IsNullOrEmpty(numbers)) return 0;
-            else if (numbers == "1,1") return 2;
+            else if (numbers.Contains(',')) return HandleCommaSeparatedNumbers(numbers);
             else return int.Parse(numbers);
+        }
+
+        private static int HandleCommaSeparatedNumbers(string numbers)
+        {
+            var nums = numbers.Split(',');
+            return int.Parse(nums[0]) + int.Parse(nums[1]);
         }
     }
 }
