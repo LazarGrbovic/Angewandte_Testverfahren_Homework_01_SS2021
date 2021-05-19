7a8451a984602ca3c7b15152d5c9212a56942aec
LazarGrbovic <lazarmgrbovic@gmail.com>
2021-05-19 14:34:27 +0200

    Added Add method

--- StringCalculator/StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -4,5 +4,9 @@ namespace StringCalculator
 {
     public static class StringCalculator
     {
+        public static int Add(string numbers)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
