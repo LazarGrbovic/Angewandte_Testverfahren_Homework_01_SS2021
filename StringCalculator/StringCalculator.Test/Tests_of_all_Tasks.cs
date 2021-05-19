93aa6861f7e68b208bfa0f1e332c6d96f7516c9e
LazarGrbovic <lazarmgrbovic@gmail.com>
2021-05-19 14:42:45 +0200

    Added second failing test of the first task

--- StringCalculator/StringCalculator.Test/Tests_of_all_Tasks.cs
+++ StringCalculator/StringCalculator.Test/Tests_of_all_Tasks.cs
@@ -8,7 +8,13 @@ namespace StringCalculator.Test
         [TestMethod]
         public void Add_Method_Returns_Zero_For_Empty_String_Input()
         {
-            Assert.AreEqual(StringCalculator.Add(""), 0);
+            Assert.AreEqual(StringCalculator.Add(""), 0);            
+        }
+
+        [TestMethod]
+        public void Add_Method_Returns_Number_For_A_Single_Number_Input()
+        {
+            Assert.AreEqual(StringCalculator.Add("1"), 1);
         }
     }
 }
