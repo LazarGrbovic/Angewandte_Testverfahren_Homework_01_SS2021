b472d4e6f9c19730c23d8b1dcc5b147b73b77292
LazarGrbovic <lazarmgrbovic@gmail.com>
2021-05-19 14:31:55 +0200

    Setup working environment
    * Created all files
    * Renamed things

---
+++ StringCalculator/StringCalculator.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator.Test
+{
+    [TestClass]
+    public class Task_1_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
