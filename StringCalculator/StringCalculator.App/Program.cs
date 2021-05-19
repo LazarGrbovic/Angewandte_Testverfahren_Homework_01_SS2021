68689468fd0a8a5b23e9a239f7cd2e6fea09e974
LazarGrbovic <lazarmgrbovic@gmail.com>
2021-05-19 18:52:25 +0200

    Added simple console application that will display the couple of Examples of the String Calculator

--- StringCalculator/StringCalculator.App/Program.cs
+++ StringCalculator/StringCalculator.App/Program.cs
@@ -6,7 +6,44 @@ namespace StringCalculator.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Welcome to the String Calculator!");
+            Console.WriteLine("Examples: ");
+            
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("1 = ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(StringCalculator.Add("1"));
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("1,1 = ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(StringCalculator.Add("1,1"));
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("//;\\n1;1 = ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(StringCalculator.Add("//;\n1;1"));
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("//[***]\\n1***1 = ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(StringCalculator.Add("//[***]\n1***1"));
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("//[***][###]\\n1***2###3 = ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(StringCalculator.Add("//[***][###]\n1***2###3"));            
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();            
         }
     }
 }
