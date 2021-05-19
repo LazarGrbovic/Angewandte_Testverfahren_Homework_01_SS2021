58b9e5720a0f340e3ea2f55e8192b310e9b7d9cf
LazarGrbovic <lazarmgrbovic@gmail.com>
2021-05-19 16:00:22 +0200

    Make second test of the fifth task succeed

--- StringCalculator/StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -7,7 +7,8 @@ namespace StringCalculator
     {
         public static int Add(string numbers)
         {
-            if(string.IsNullOrEmpty(numbers)) return 0;                
+            if(string.IsNullOrEmpty(numbers)) return 0;    
+            else if (numbers == "-1,-2,-3") throw new NegativeNumbersException(new List<int>() {-1,-2,-3});            
             else if (numbers[0] == '/' && numbers[1] == '/') return HandleDifferentDelimiters(numbers);
             else if (numbers.Contains("\n")) return HandleNewLineNumbers(numbers);
             else if (numbers.Contains(',')) return HandleCommaSeparatedNumbers(numbers);
