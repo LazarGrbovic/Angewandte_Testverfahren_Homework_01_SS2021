ad009884845ef3f42dfca3d3119f806be0c3ae22
LazarGrbovic <lazarmgrbovic@gmail.com>
2021-05-19 15:25:41 +0200

    Refactored Implementation
    * Now different delimiters can be handled

--- StringCalculator/StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -7,10 +7,18 @@ namespace StringCalculator
         public static int Add(string numbers)
         {
             if(string.IsNullOrEmpty(numbers)) return 0;    
-            else if (numbers == "//;n1;2") return 3;
+            else if (numbers[0] == '/' && numbers[1] == '/') return HandleDifferentDelimiters(numbers);
             else if (numbers.Contains("\n")) return HandleNewLineNumbers(numbers);
             else if (numbers.Contains(',')) return HandleCommaSeparatedNumbers(numbers);
             else return int.Parse(numbers);
+        }
+
+        private static int HandleDifferentDelimiters(string numbers)
+        {
+                var delimiter = numbers[2];
+                var refactoredNumbers =  numbers.Remove(0,4);
+                var numbersWithRefactoredDelimiter = refactoredNumbers.Replace("\n", ",").Replace("\r","").Replace(delimiter, ',');
+                return HandleCommaSeparatedNumbers(numbersWithRefactoredDelimiter);
         }
 
         private static int HandleNewLineNumbers(string numbers)
